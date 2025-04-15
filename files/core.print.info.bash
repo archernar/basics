cc3bb548d68969d9367b340351b83f4f57b71b8a
michael <michael@fastlight.com>
2025-04-15 16:54:00 -0400

    Updated ./files amd index.html

--- files/core.print.info.bash
+++ files/core.print.info.bash
@@ -1,7 +1,7 @@
 function core.print.info() {
 	if core._should_print_color 1; then
-		printf "\033[1;32m%s:\033[0m %s\n" 'Info' "$msg"
+		printf "\033[1;32m%s: %s:\033[0m %s\n" 'Info' "$FUNCNAME" "$msg"
 	else
-		printf "%s: %s\n" 'Info' "$msg"
+		printf "%s: %s: %s\n" 'Info' "$FUNCNAME" "$msg"
 	fi
 }
