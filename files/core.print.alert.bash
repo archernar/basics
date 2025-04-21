79c493e205f9f68ef0a79e0fc1ef922d796bc965
michael <michael@fastlight.com>
2025-04-21 09:00:44 -0400

    Updated ./files amd index.html

--- files/core.print.alert.bash
+++ files/core.print.alert.bash
@@ -2,7 +2,7 @@ function core.print.alert() {
     local f=${FUNCNAME[1]}
     local ff=$(printf "%-32s\n" "$f")
 	if core._should_print_color 2; then
-        util.print.red() $(printf "%s: %s: %s" 'Alt' "$ff" "$msg")
+        util.print.red $(printf "%s: %s: %s" 'Alt' "$ff" "$msg")
         #printf "\033[1;31m%s: %s: %s\033[0m\n" 'Alt' "$ff" "$msg" >&2
 	else
         printf "%s: %s: %s\n" 'Alt' "$ff" "$msg" >&2
