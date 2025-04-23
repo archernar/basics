794974fdf609941e86b6afc5b5ba6481dd962299
michael <michael@fastlight.com>
2025-04-22 21:12:19 -0400

    Updated ./files amd index.html

---
+++ files/ansi.constants.bash
@@ -0,0 +1,21 @@
+function ansi.constants() {
+# Color Codes:
+#   -   black
+#   -   red
+#   -   green
+#   -   yellow
+#   -   blue
+#   -   magenta
+#   -   cyan
+#   -   white
+#   -   default (for resetting to terminal default)
+#
+# Attribute Codes:
+#   -   normal
+#   -   bold
+#   -   dim
+#   -   underline
+#   -   blink
+#   -   reverse
+#   -   hidden
+}
