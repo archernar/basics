e64921a0689568d7a86036d238f0a56090d547e3
michael <michael@fastlight.com>
2025-04-26 13:07:02 -0400

    Updated ./files and index.html

--- files/core.print.info.bash
+++ files/core.print.info.bash
@@ -1,7 +1,6 @@
 function core.print.info() {
-    local f=${FUNCNAME[1]}
     local ff=$(printf "%-32s\n" "${FUNCNAME[1]}")
-    local sz="$(printf "%s: %s %s: %s" "Info" "$SILENT" "$ff" "$msg")"
+    local sz="$(printf "%s: %s %s: %s" "Info" "$SILENT" "$(printf "%-32s\n" "${FUNCNAME[1]}" "$msg")"
     if [ "$DEBUG" -gt 0 ]; then
         if core._should_print_color 1; then
             util.println.green "$sz"
