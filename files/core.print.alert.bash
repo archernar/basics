d0b934ef0e23cd116c289621492425671216f706
michael <michael@fastlight.com>
2025-04-26 12:59:11 -0400

    Updated ./files and index.html

--- files/core.print.alert.bash
+++ files/core.print.alert.bash
@@ -1,5 +1,5 @@
 function core.print.alert() {
-    local sz="$(printf "%s: %s %s: %s" "Alrt" "$SILENT" $(printf "%-32s\n" "${FUNCNAME[1]}" "$msg")"
+    local sz="$(printf '%s: %s %s: %s' 'Alrt' '$SILENT' $(printf '%-32s\n' '${FUNCNAME[1]}' '$msg')"
 	if core._should_print_color 2; then
         util.println.magenta "$sz"
 	else
