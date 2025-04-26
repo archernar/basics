ac3db9a80d21f6012bd7ae973d0d94f908a88321
michael <michael@fastlight.com>
2025-04-26 12:51:55 -0400

    Updated ./files and index.html

--- files/core.print.alert.bash
+++ files/core.print.alert.bash
@@ -2,8 +2,8 @@ function core.print.alert() {
     local f=${FUNCNAME[1]}
     local ff=$(printf "%-32s\n" "$f")
 	if core._should_print_color 2; then
-        util.println.magenta "$(printf "%s: %s: %s" "Alt" "$ff" "$msg")" >&2
+        util.println.magenta "$(printf "%s: %s %s: %s" "Alt" "$SILENT" "$ff" "$msg")" >&2
 	else
-        util.println.white "$(printf "%s: %s: %s" "Alt" "$ff" "$msg")" >&2
+        util.println.white "$(printf "%s: %s %s: %s" "Alt" "$SILENT" "$ff" "$msg")" >&2
 	fi
 }
