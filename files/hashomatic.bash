856680aa13727f805fc8dae019c3cd17fab685ce
michael <michael@fastlight.com>
2025-04-27 13:17:46 -0400

    Updated ./files and index.html

---
+++ files/hashomatic.bash
@@ -0,0 +1,21 @@
+function hashomatic() {
+  local tmp1=$(mktemp)
+  if [[ -z "$filename" ]]; then
+    fecho "Error: Filename not provided."
+    return 1
+  fi
+  if [[ ! -f "$filename" ]]; then
+    fecho "Error: File '$filename' not found."
+    return 1
+  fi
+  if [ $(isSameHash "$filename") == "0" ]; then
+    core.print.info "HashoMatic $filename"
+    grep -v DATEOMATIC "$filename" | grep -v HASHOMATIC > $tmp1
+    IHASH=$(md5sum "tmp1" | awk '{print $1}')
+    core.print.echo "\" *********************************************************DATEOMATIC: $D"      > $tmp1
+    core.print.echo "\" *********************************************************HASHOMATIC: $IHASH" >> $tmp1
+    cat $tmp1  > "$filename"
+    git add "$filename"
+    rm -f "$tmp1" >/dev/null 2>&1
+  fi
+}
