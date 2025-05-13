291ef2dbf1fe39d92f907f647d63a6ce892d5e97
michael <michael@fastlight.com>
2025-05-13 08:39:49 -0400

    Updated ./files and index.html

--- files/ansi.attributes.bash
+++ files/ansi.attributes.bash
@@ -1,13 +1,13 @@
 function ansi.attributes() {
 #   attribute: The attribute name (e.g., bold, underline).
     case "$attribute" in
-        normal) printf "\033[0m" ;;
-        bold) printf "\033[1m" ;;
-        dim) printf "\033[2m" ;;
-        underline) printf "\033[4m" ;;
-        blink) printf "\033[5m" ;;
-        reverse) printf "\033[7m" ;;
-        hidden) printf "\033[8m" ;;
+        normal)     printf "\033[0m" ;;
+        bold)       printf "\033[1m" ;;
+        dim)        printf "\033[2m" ;;
+        underline)  printf "\033[4m" ;;
+        blink)      printf "\033[5m" ;;
+        reverse)    printf "\033[7m" ;;
+        hidden)     printf "\033[8m" ;;
         *) echo "Invalid attribute: $attribute" >&2; return 1 ;;
     esac
 }
--- files/ansi.backgroundcolor.bash
+++ files/ansi.backgroundcolor.bash
@@ -1,15 +1,15 @@
 function ansi.backgroundcolor() {
 #   color: The color name (e.g., black, red, green).
     case "$color" in
-        black) printf "\033[40m" ;;
-        red) printf "\033[41m" ;;
-        green) printf "\033[42m" ;;
-        yellow) printf "\033[43m" ;;
-        blue) printf "\033[44m" ;;
-        magenta) printf "\033[45m" ;;
-        cyan) printf "\033[46m" ;;
-        white) printf "\033[47m" ;;
-        default) printf "\033[49m" ;; # Use 49 for default
+        black)      printf "\033[40m" ;;
+        red)        printf "\033[41m" ;;
+        green)      printf "\033[42m" ;;
+        yellow)     printf "\033[43m" ;;
+        blue)       printf "\033[44m" ;;
+        magenta)    printf "\033[45m" ;;
+        cyan)       printf "\033[46m" ;;
+        white)      printf "\033[47m" ;;
+        default)    printf "\033[49m" ;; # Use 49 for default
         *) echo "Invalid color: $color" >&2; return 1 ;;
     esac
 }
