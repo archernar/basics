6abb1a0071f88099ca65edc007d7c672e9d697f1
michael <michael@fastlight.com>
2025-05-14 08:19:48 -0400

    Updated ./files and index.html

--- files/core.print.advice.bash
+++ files/core.print.advice.bash
@@ -1,6 +1,7 @@
 function core.print.advice() {
          local lvl="$LL3A"
     local ff=$(printf "%-32s\n" "${FUNCNAME[1]}")
+    ff=$(printf "%-32s\n" "${FUNCNAME[2]}${FUNCNAME[1]}")
     local sz="$(printf "%s %s: %s %s" "Advc$lvl" "$SILENT" "$ff" "$msg")"
     if [ "$LOGLEVEL" -gt $lvl ]; then
         util.print.color blue "$sz"
