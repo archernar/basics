3e08ee1373cc6c8fdfca8c2227348cecea68bf71
michael <michael@fastlight.com>
2025-06-02 20:29:11 -0400

    Updated ./files and index.html

--- files/printAlways.bash
+++ files/printAlways.bash
@@ -1,6 +1,3 @@
 function printAlways() {
-         LASTCONSOLEMESSAGE="$1"
-    local ff=$(printf "%-12s\n" "${FUNCNAME[1]}")
-    local sz="$(printf "%s %s: %s %s" "      " "$SILENT" "$ff" "$msg")"
-    printAtLevel "$sz" $LL0 "    " yellow
+    printAtLevel "$msg" $LL0 "    " yellow
 }
